9cb481cd420787a9b3a52f7113127f2abae1a662
Andras Salamon <andras.salamon@melda.info>
2018-11-03 08:25:08 +0100

    using temp directory

--- exposure_fusion.sh
+++ exposure_fusion.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -euo pipefail
+
+TMPROOTDIR="."
+TMPDIR="${TMPROOTDIR}/EXPOSUREFUSION.$$"
+
 V=$(basename "$1" .dng)
 if [[ ${V} =~ ^([a-z_0-9]*)([0-9]{4}) ]]; then
  NUM=${BASH_REMATCH[2]}
@@ -8,22 +12,25 @@ else
  echo Invalid filename
  exit 1
 fi
+
+mkdir "$TMPDIR" || error "CANNOT CREATE TEMPORARY FILE DIRECTORY"
+
 x1=$(printf '%04d' $(( NUM )));
 x2=$(printf '%04d' $(( NUM+1 )));
 x3=$(printf '%04d' $(( NUM+2 )));
 efname=${PREFIX}${x1}_ef
 greenvalue=${3:-}
 colortemp=${2-5500}
+
 echo Using color temperature "${colortemp}"
 [[ ! -z ${greenvalue} ]] && greenparam="--green ${greenvalue}" && echo Using green "${greenvalue}"
-parallel --no-notice ufraw-batch --temperature="${colortemp}" "${greenparam}" --out-type=png ::: "${PREFIX}${x1}.dng" "${PREFIX}${x2}.dng" "${PREFIX}${x3}.dng"
-align_image_stack -s 3 -a "ais_${PREFIX}${x1}_" "${PREFIX}${x1}.png" "${PREFIX}${x2}.png" "${PREFIX}${x3}.png"
-enfuse -o "${efname}.tif" "ais_${PREFIX}${x1}"_*.tif
-convert "${efname}".{tif,jpg}
-exiftool -TagsFromFile "${PREFIX}${x1}.png" -all:all "${efname}.jpg"
-exiftool '-DateTimeOriginal>FileModifyDate' "${efname}.jpg"
-exiftool -P -keywords+="exposure fusion" "${efname}.jpg"
-rm "ais_${PREFIX}${x1}"_*.tif
-rm "${efname}.tif"
-rm "${efname}.jpg_original"
-rm "${PREFIX}${x1}.png" "${PREFIX}${x2}.png" "${PREFIX}${x3}.png"
+parallel --no-notice ufraw-batch --temperature="${colortemp}" "${greenparam}" --out-type=png --out-path ${TMPDIR} ::: "${PREFIX}${x1}.dng" "${PREFIX}${x2}.dng" "${PREFIX}${x3}.dng"
+
+align_image_stack -s 3 -a "${TMPDIR}/ais_${PREFIX}${x1}_" "${TMPDIR}/${PREFIX}"{"${x1}","${x2}","${x3}"}.png
+enfuse -o "${TMPDIR}/${efname}.tif" "${TMPDIR}/ais_${PREFIX}${x1}"_*.tif
+convert "${TMPDIR}/${efname}".{tif,jpg}
+exiftool -TagsFromFile "${TMPDIR}/${PREFIX}${x1}.png" -all:all "${TMPDIR}/${efname}.jpg"
+exiftool '-DateTimeOriginal>FileModifyDate' "${TMPDIR}/${efname}.jpg"
+exiftool -P -keywords+="exposure fusion" "${TMPDIR}/${efname}.jpg"
+mv "${TMPDIR}/${efname}.jpg" .
+rm -rf "${TMPDIR}"
